3f7e332c807cc042fd70d91b96b5164601052af4
Mark Zuschlag <mzuschlag@MBP-0518.local>
2019-02-20 22:16:30 -0800

    allow users to quit

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use std::io::{self, BufRead};
 mod lexer;
 
 fn main() {
+    println!("Welcom to Rustkalkulator!");
+    println!("Press 'q' to quit");
     loop {
         let stdin = io::stdin();
         for line in stdin.lock().lines() {
             let string = line.unwrap();
+            if string == "q" {
+                println!("Goodbye!");
+                return;
+            }
             let chars = string.chars();
             let mut tokenizer = lexer::Tokens::new(chars);
             tokenizer.tokenize();
